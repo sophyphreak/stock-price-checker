6ea411c22c1e206a8fa49590904b64440e94827b
Andrew <arhorn@smcm.edu>
2019-02-19 02:30:09 +0700

    added Stock model to api

--- routes/api.js
+++ routes/api.js
@@ -9,14 +9,17 @@
 'use strict';
 
 require('../db/mongoose');
+const { Stock } = require('../models/stock');
 
 module.exports = app => {
   app.route('/api/stock-prices').get(async (req, res) => {
     let stockData = { stockData: {} };
-    const { stock } = req.query.stock;
+    const { stock, like } = req.query;
     if (stock.isArray()) {
       const stockOne = stock[0];
       const stockTwo = stock[1];
+      // add more
+    } else {
     }
 
     res.send(JSON.stringify(stockData));
