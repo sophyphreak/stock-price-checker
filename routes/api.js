f0ee9acfad0bc2c31e3fa0d4ad86cd5b5c73a8f6
Andrew <arhorn@smcm.edu>
2019-02-18 17:59:09 +0700

    converted var to const

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,18 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const expect = require('chai').expect;
+const MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+module.exports = function(app) {
+  app.route('/api/stock-prices').get(function(req, res) {});
 };
