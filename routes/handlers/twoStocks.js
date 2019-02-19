55e102d3bd854a6d0749bef4157be262b2469329
Andrew <arhorn@smcm.edu>
2019-02-20 01:12:05 +0700

    started doing twoStocks.js

--- routes/handlers/twoStocks.js
+++ routes/handlers/twoStocks.js
@@ -1,7 +1,13 @@
 // if (like) { }
 // if (databaseHasSymbol) { }
 // 2x2 = 4 different paths
+const { getPrice } = require('./getPrice');
+const { getStockLikes } = require('./getStockLikes');
+const { updateStockWithLike } = require('./updateStockWithLike');
 
-const twoStocks = (stock, like) => {};
+const twoStocks = (stock, like) => {
+  const stock1 = stock[0].toUpperCase();
+  const stock2 = stock[1].toUpperCase();
+};
 
 module.exports = { twoStocks };
