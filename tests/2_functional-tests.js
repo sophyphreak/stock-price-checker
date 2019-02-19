1cbed3b3cfbf8f68492365b4cbdff78d8e45f876
Andrew <arhorn@smcm.edu>
2019-02-20 01:11:32 +0700

    Added final two tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,13 +68,71 @@ suite('Functional Tests', () => {
           assert.equal(stock, 'GOOG', 'should be capitalized ticker symbol');
           assert.isNumber(parseFloat(price));
           assert.isNumber(likes);
-          assert.equal(likes, 2, 'there should be one like');
+          assert.equal(likes, 2, 'there should be two likes');
           done();
         });
     });
 
-    test('2 stocks', done => {});
+    test('2 stocks', done => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['goog', 'msft'] })
+        .end((err, res) => {
+          const data = JSON.parse(res.body);
+          const stock1data = data.stockData[0];
+          const stock2data = data.stockData[1];
+          let { stock, price, rel_likes } = stock1data;
+          assert.isString(stock);
+          assert.equal(stock, 'GOOG', 'should be capitalized ticker symbol');
+          assert.isNumber(parseFloat(price));
+          assert.isNumber(rel_likes);
+          assert.equal(rel_likes, 2, 'there should be two relational likes');
+          stock = stock2data.stock;
+          price = stock2data.price;
+          rel_likes = stock2data.rel_likes;
+          assert.isString(stock);
+          assert.equal(stock, 'MSFT', 'should be capitalized ticker symbol');
+          assert.isNumber(parseFloat(price));
+          assert.isNumber(rel_likes);
+          assert.equal(
+            rel_likes,
+            -2,
+            'there should be negative two relational likes'
+          );
+          done();
+        });
+    });
 
-    test('2 stocks with like', done => {});
+    test('2 stocks with like', done => {
+      chai
+        .request(server)
+        .get('/api/stock-prices')
+        .query({ stock: ['goog', 'msft'], like: true })
+        .end((err, res) => {
+          const data = JSON.parse(res.body);
+          const stock1data = data.stockData[0];
+          const stock2data = data.stockData[1];
+          let { stock, price, rel_likes } = stock1data;
+          assert.isString(stock);
+          assert.equal(stock, 'GOOG', 'should be capitalized ticker symbol');
+          assert.isNumber(parseFloat(price));
+          assert.isNumber(rel_likes);
+          assert.equal(rel_likes, 2, 'there should be two relational likes');
+          stock = stock2data.stock;
+          price = stock2data.price;
+          rel_likes = stock2data.rel_likes;
+          assert.isString(stock);
+          assert.equal(stock, 'MSFT', 'should be capitalized ticker symbol');
+          assert.isNumber(parseFloat(price));
+          assert.isNumber(rel_likes);
+          assert.equal(
+            rel_likes,
+            -2,
+            'there should be negative two relational likes'
+          );
+          done();
+        });
+    });
   });
 });
