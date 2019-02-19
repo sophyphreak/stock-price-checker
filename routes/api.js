1f02336cd03177e627c6e70a84520054f68e295b
Andrew <arhorn@smcm.edu>
2019-02-20 00:35:34 +0700

    moved logic to handlers, one stock input now functions correctly

--- routes/api.js
+++ routes/api.js
@@ -10,44 +10,12 @@
 
 require('../db/mongoose');
 const { Stock } = require('../models/stock');
+const { handlers } = require('./handlers/handlers');
 
 module.exports = app => {
   app.route('/api/stock-prices').get(async (req, res) => {
-    let stockData = { stockData: {} };
     const { stock, like } = req.query;
-    if (stock.isArray()) {
-      const stockOne = stock[0];
-      const stockTwo = stock[1];
-      // add more
-    } else {
-      if (like) {
-        let { _id, likes } = await Stock.findOneAndUpdate(
-          { symbol: stock },
-          { $inc: { likes: 1 } },
-          { new: true }
-        );
-      } else {
-        let { _id, likes } = await Stock.findOne({ symbol: stock });
-      }
-      if (!_id) {
-        if (like) {
-          const doc = new Stock({
-            symbol: stock,
-            likes: 1
-          });
-          doc.save();
-        } else {
-          const doc = new Stock({
-            symbol: stock,
-            likes: 0
-          });
-          doc.save();
-        }
-        // more logic. how do i get likes out? etc etc.
-        // would be nice to refactor this into cleaner helper functions.
-      }
-    }
-
-    res.send(JSON.stringify(stockData));
+    const result = await handlers(stock, like);
+    res.json(result);
   });
 };
